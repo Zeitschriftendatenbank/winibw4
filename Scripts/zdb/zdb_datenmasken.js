9d7bdbc01673149f66f36e8f5e9b344b4fc264b0
Carsten Klee <carsten.klee@sbb.spk-berlin.de>
2025-10-21 16:27:47 +0200

    Datenmaske Scripte vereinheitlichen

--- Scripts/zdb/zdb_datenmasken.js
+++ Scripts/zdb/zdb_datenmasken.js
@@ -1,32 +1,30 @@
 function zdb_DatenmaskeAbxz()
 {
-     if (__pruefeZDB()) {
-         __DatenmaskeEinfuegen("maskeAbxz_zdb.txt");
-     }
+     __DatenmaskeEinfuegen("maskeAbxz_zdb.txt");
 }
 
 
 function zdb_DatenmaskeAbxzTr()
 {
-    __DatenmaskeEinfuegen("\\datenmasken_zdb\\maskeAbxzTr_zdb.txt");
+    __DatenmaskeEinfuegen("maskeAbxzTr_zdb.txt");
 }
 
 function zdb_DatenmaskeObxz()
 {
-    __DatenmaskeEinfuegen("\\datenmasken_zdb\\maskeObxz_zdb.txt");
+    __DatenmaskeEinfuegen("maskeObxz_zdb.txt");
 }
 
 function zdb_DatenmaskeAdxz()
 {
-    __DatenmaskeEinfuegen("\\datenmasken_zdb\\maskeAdxz_zdb.txt");
+    __DatenmaskeEinfuegen("maskeAdxz_zdb.txt");
 }
 
 function zdb_DatenmaskeOdxz()
 {
-    __DatenmaskeEinfuegen("\\datenmasken_zdb\\maskeOdxz_zdb.txt");
+    __DatenmaskeEinfuegen("maskeOdxz_zdb.txt");
 }
-
-/*function zdb_nutzerMaske() {
+/*
+function zdb_nutzerMaske() {
     var maskenNr = application.getProfileString("zdb.userdata", "maske", "");
     __DatenmaskeEinfuegen(maskenNr);
 }*/
