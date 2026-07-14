c442263136cdfad59046225fe706d45bb76e699d
Carsten Klee <carsten.klee@sbb.spk-berlin.de>
2026-07-14 13:50:11 +0200

    moved functions to misc

--- Scripts/zdb_dialoge.js
+++ Scripts/zdb_dialoge.js
@@ -1,36 +1,3 @@
-if (typeof Array.isArray !== 'function') {
-    Array.isArray = function (arr) {
-        return Object.prototype.toString.call(arr) === "[object Array]";
-    };
-}
-
-if (!Array.prototype.indexOf) {
-    Array.prototype.indexOf = function (searchElement, fromIndex) {
-        var k;
-        if (this == null) {
-            throw new TypeError('"this" is null or not defined');
-        }
-        var o = Object(this);
-        var len = o.length >>> 0;
-        if (len === 0) {
-            return -1;
-        }
-        var n = fromIndex | 0;
-        if (n >= len) {
-            return -1;
-        }
-        k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
-        while (k < len) {
-            if (k in o && o[k] === searchElement) {
-                return k;
-            }
-            k++;
-        }
-        return -1;
-    };
-}
-
-
 function zdb_AutomatischeSuchBox() {
     if (false == ZDB.checkScreen(['MT', 'IT', 'IE'], 'AutomatischeSuchBox')) return false;
     ZDB.anfangsfenster = activeWindow.windowID; // globale Variable, die vom Skript HoleIDN verwendet wird
